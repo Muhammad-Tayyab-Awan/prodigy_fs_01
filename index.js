71c2c169f4ae26e793029d6536e0dcb6fc24dd33
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-11 23:19:46 +0500

    feat: added logout route to the application

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const connectDB = require("./utils/connectDB");
 const verifyLogin = require("./middleware/verifyLogin.js");
 const registerRoute = require("./routes/auth/register.js");
 const loginRoute = require("./routes/auth/login.js");
+const logoutRoute = require("./routes/auth/logout.js");
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
@@ -32,6 +33,7 @@ app.get("/", (req, res) => {
 
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
+app.use("/logout", logoutRoute);
 
 app.all(/(.*)/, (req, res) => {
   const pageRequested = req.path.slice(1);
