365a77b97d8ba885917d2900a04be93333687f08
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-11 12:16:09 +0500

    feat: implement verifyLogin middleware for JWT authentication

---
+++ middleware/verifyLogin.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+const User = require("../models/Users.js");
+
+const verifyLogin = (req, res, next) => {
+  try {
+    const { cookies } = req;
+    const authToken = cookies["roluthentify_auth_token"];
+    if (!authToken) {
+      req.userStatus = { loggedIn: false };
+      return next();
+    }
+    jwt.verify(authToken, jwtSecret, async (err, decodedToken) => {
+      if (err) {
+        req.userStatus = { loggedIn: false };
+        return next();
+      }
+      const { userId } = decodedToken;
+      const userExist = await User.findById(userId);
+      if (!(userExist && userExist.verified)) {
+        req.userStatus = { loggedIn: false };
+        return next();
+      }
+      req.userStatus = { loggedIn: true, userId: userId };
+      return next();
+    });
+  } catch (error) {
+    res.render("error", {
+      error: "Sever side error occurred",
+      message: error
+    });
+  }
+};
+module.exports = verifyLogin;
