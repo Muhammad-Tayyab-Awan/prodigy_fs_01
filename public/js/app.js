db1649df8461ded2681c627c12db11d5cf6b1fbd
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-11 22:05:06 +0500

    feat: added form validation for password matching on register submission

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 const accountToggler = document.querySelector(".userAccountToggler");
 const userDropDown = document.querySelector("#user-dropdown");
 const reloadBtns = document.querySelectorAll(".reloadBtn");
+const register = document.querySelector("#register");
 const toggleUserDropDown = () => {
   userDropDown.classList.toggle("hidden");
   userDropDown.classList.toggle("flex");
@@ -11,4 +12,16 @@ const reloadPage = () => {
 accountToggler?.addEventListener("click", toggleUserDropDown);
 reloadBtns?.forEach((btn) => {
   btn.addEventListener("click", reloadPage);
+});
+register?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const inputFields = e.target.getElementsByTagName("input");
+  const password = inputFields[2].value;
+  const cPassword = inputFields[3].value;
+  if (password !== cPassword) {
+    alert("Passwords do not match");
+    e.target.reset();
+    return;
+  }
+  e.target.submit();
 });
