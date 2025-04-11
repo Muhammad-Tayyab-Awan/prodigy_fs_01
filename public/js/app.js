98385d48d571f63333013f89e11213a11c786298
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-11 09:25:33 +0500

    feat: added reload functionality to user dropdown interactions

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,14 @@
 const accountToggler = document.querySelector(".userAccountToggler");
 const userDropDown = document.querySelector("#user-dropdown");
+const reloadBtns = document.querySelectorAll(".reloadBtn");
 const toggleUserDropDown = () => {
   userDropDown.classList.toggle("hidden");
   userDropDown.classList.toggle("flex");
 };
-accountToggler.addEventListener("click", toggleUserDropDown);
+const reloadPage = () => {
+  window.location.reload();
+};
+accountToggler?.addEventListener("click", toggleUserDropDown);
+reloadBtns?.forEach((btn) => {
+  btn.addEventListener("click", reloadPage);
+});
