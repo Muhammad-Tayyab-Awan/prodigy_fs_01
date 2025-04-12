b4775e3575ec41c5e16217b83c71b7073e515dba
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-12 22:51:25 +0500

    feat: added user deletion route with validation and error handling

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { param, validationResult } = require("express-validator");
 const User = require("../models/Users.js");
 
 router.get("/", async (req, res) => {
@@ -36,5 +37,37 @@ router.get("/users", async (req, res) => {
     });
   }
 });
+
+router.get(
+  "/users/delete/:userId",
+  param("userId").isMongoId(),
+  async (req, res) => {
+    try {
+      const { userStatus } = req;
+      if (!(userStatus.role === "admin"))
+        return res.redirect(`/${userStatus.loggedIn ? "profile" : "login"}`);
+      const result = validationResult(req);
+      if (!result.isEmpty())
+        return res.status(404).render("error", {
+          error: "Invalid request",
+          message: `${result.errors.length} invalid values, please provide correct values`
+        });
+      const { userId } = req.params;
+      const userExist = await User.findById(userId);
+      if (!userExist)
+        return res.render("error", {
+          error: "Invalid request",
+          message: "No user exist with such id"
+        });
+      await User.findByIdAndDelete(userId);
+      res.redirect("/admin/users");
+    } catch (error) {
+      res.render("error", {
+        error: "Server side error occurred",
+        message: error
+      });
+    }
+  }
+);
 
 module.exports = router;
