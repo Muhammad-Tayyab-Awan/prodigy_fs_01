45ca7e2a52c7b41d7c465dfcc6f6f7120acc2180
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-12 16:05:14 +0500

    feat: added user update functionality with validation and error handling

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { body, validationResult } = require("express-validator");
 const User = require("../models/Users.js");
 
 router.get("/", async (req, res) => {
@@ -31,5 +32,54 @@ router.get("/delete", async (req, res) => {
     });
   }
 });
+
+router.post(
+  "/update",
+  [
+    body("username")
+      .matches(/^(?=.*[a-z])(?=.*\d)[a-z\d]+$/)
+      .isLength({ min: 6, max: 20 })
+      .optional(),
+    ,
+    body("email").isEmail().optional()
+  ],
+  async (req, res) => {
+    try {
+      const { userStatus } = req;
+      if (!userStatus.loggedIn) return res.redirect("/login");
+      const result = validationResult(req);
+      if (!result.isEmpty())
+        return res.status(404).render("error", {
+          error: "Invalid request",
+          message: `${result.errors.length} invalid values, please provide correct values`
+        });
+      for (const element in req.body) {
+        if (!(element === "username" || element === "email"))
+          delete req.body[element];
+      }
+      const usernameExist = await User.findOne({ username: req.body.username });
+      const userEmailExist = await User.findOne({ email: req.body.email });
+      if (userEmailExist && usernameExist)
+        return res.status(404).render("error", {
+          error: "Invalid request",
+          message: `User already exist with this email and username`
+        });
+      if (usernameExist || userEmailExist)
+        return res.status(404).render("error", {
+          error: "Invalid request",
+          message: `User already exist with this ${
+            usernameExist ? "username" : "email"
+          }`
+        });
+      await User.findByIdAndUpdate(userStatus.userId, req.body);
+      res.redirect("/profile");
+    } catch (error) {
+      res.render("error", {
+        error: "Server side error occurred",
+        message: error
+      });
+    }
+  }
+);
 
 module.exports = router;
