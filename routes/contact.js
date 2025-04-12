a998bb41e0eabfba5748418804448d62e546edfa
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-12 09:54:09 +0500

    feat: implemented POST route for contact form with validation and error handling

--- routes/contact.js
+++ routes/contact.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const { body, validationResult } = require("express-validator");
+const ContactForm = require("../models/ContactForm.js");
 
 router.get("/", async (req, res) => {
   try {
@@ -11,5 +13,35 @@ router.get("/", async (req, res) => {
     });
   }
 });
+
+router.post(
+  "/",
+  [
+    body("name")
+      .matches(/^[A-Za-z]+(?: [A-Za-z]+)*$/)
+      .isLength({ max: 30, min: 2 }),
+    body("email").isEmail(),
+    body("message").isLength({ min: 50, max: 500 })
+  ],
+  async (req, res) => {
+    try {
+      console.log(req.body);
+      const result = validationResult(req);
+      if (!result.isEmpty())
+        return res.status(404).render("error", {
+          error: "Invalid request",
+          message: `${result.errors.length} invalid values, please provide correct values`
+        });
+      const { name, email, message } = req.body;
+      await ContactForm.create({ name: name, email: email, message: message });
+      res.redirect("/contact");
+    } catch (error) {
+      res.render("error", {
+        error: "Server side error occurred",
+        message: error
+      });
+    }
+  }
+);
 
 module.exports = router;
