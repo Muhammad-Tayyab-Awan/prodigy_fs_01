0eff03b46a56001c25615a9b388c59ced20d1933
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-12 21:59:53 +0500

    feat: enhanced admin route to include user statistics

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
+const User = require("../models/Users.js");
 
 router.get("/", async (req, res) => {
   try {
     const { userStatus } = req;
     if (!(userStatus.role === "admin"))
       return res.redirect(`/${userStatus.loggedIn ? "profile" : "login"}`);
+    let totalMembers = await User.find();
+    totalMembers = totalMembers.length;
+    let verifiedUsers = await User.find({ verified: true, role: "user" });
+    verifiedUsers = verifiedUsers.length;
+    let totalAdmins = await User.find({ role: "admin" });
+    totalAdmins = totalAdmins.length;
+    userStatus.data = { totalMembers, verifiedUsers, totalAdmins };
     res.render("admin", userStatus);
   } catch (error) {
     res.render("error", {
