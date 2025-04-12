43034096cec769ed39cac56dd35cc2f307068870
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-12 13:14:56 +0500

    feat: refactored account management functionality and add modal toggles

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,9 @@ const userDropDown = document.querySelector("#user-dropdown");
 const reloadBtns = document.querySelectorAll(".reloadBtn");
 const register = document.querySelector("#register");
 const year = document.querySelector("#year");
+const deleteAccountBtns = document.querySelectorAll(".deleteAccountBtn");
+const updateAccountBtn = document.querySelector("#updateAccountBtn");
+const deleteModal = document.querySelector("#deleteModal");
 if (year) year.innerHTML = new Date().getFullYear();
 const toggleUserDropDown = () => {
   userDropDown.classList.toggle("hidden");
@@ -11,11 +14,7 @@ const toggleUserDropDown = () => {
 const reloadPage = () => {
   window.location.reload();
 };
-accountToggler?.addEventListener("click", toggleUserDropDown);
-reloadBtns?.forEach((btn) => {
-  btn.addEventListener("click", reloadPage);
-});
-register?.addEventListener("submit", async (e) => {
+const submitForm = (e) => {
   e.preventDefault();
   const inputFields = e.target.getElementsByTagName("input");
   const password = inputFields[2].value;
@@ -26,4 +25,19 @@ register?.addEventListener("submit", async (e) => {
     return;
   }
   e.target.submit();
+};
+const toggleDeleteModal = () => {
+  deleteModal.classList.toggle("hidden");
+  deleteModal.classList.toggle("flex");
+};
+const toggleUpdateModal = (e) => {};
+accountToggler?.addEventListener("click", toggleUserDropDown);
+reloadBtns?.forEach((btn) => {
+  btn.addEventListener("click", reloadPage);
 });
+register?.addEventListener("submit", submitForm);
+deleteAccountBtns?.forEach((deleteAccountBtn) => {
+  deleteAccountBtn.addEventListener("click", toggleDeleteModal);
+});
+
+updateAccountBtn?.addEventListener("click", toggleUpdateModal);
