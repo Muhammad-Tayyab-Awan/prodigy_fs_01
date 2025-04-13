b6a1c3f402152310826e4752bf25e947460aebdd
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-13 09:20:27 +0500

    feat: added route for update guide with admin validation and error handling

--- routes/admin.js
+++ routes/admin.js
@@ -311,4 +311,18 @@ router.get(
   }
 );
 
+router.get("/update-guide", async (req, res) => {
+  try {
+    const { userStatus } = req;
+    if (!(userStatus.role === "admin"))
+      return res.redirect(`/${userStatus.loggedIn ? "profile" : "login"}`);
+    res.render("update-guide", userStatus);
+  } catch (error) {
+    res.render("error", {
+      error: "Server side error occurred",
+      message: error
+    });
+  }
+});
+
 module.exports = router;
